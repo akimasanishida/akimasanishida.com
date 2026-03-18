4345d0da309d99f466e4ca6745d9bb856ea64ecd
akimasanishida <90087433+akimasanishida@users.noreply.github.com>
2026-03-18 21:16:15 +0900

    motion変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,36 @@
 import Main from "@/components/Main";
 import Header from "@/components/Header";
 import Section from "@/components/Section";
-import { AboutSection, ContactSection, PagesSection, ProfileSection } from "./page-client";
+import {
+  AboutSection,
+  ContactSection,
+  PagesSection,
+  ProfileSection,
+} from "./page-client";
+import { LazyMotion, domAnimation } from "motion/react";
 
 export default function Home() {
   return (
     <Main>
       <Header title="西田 明正（Akimasa NISHIDA）" subtitle="地球人です" />
 
-      <Section id="profile">
-        <ProfileSection />
-      </Section>
+      <LazyMotion features={domAnimation}>
+        <Section id="profile">
+          <ProfileSection />
+        </Section>
 
-      <Section id="pages">
-        <PagesSection />
-      </Section>
+        <Section id="pages">
+          <PagesSection />
+        </Section>
 
-      <Section id="about">
-        <AboutSection />
-      </Section>
+        <Section id="about">
+          <AboutSection />
+        </Section>
 
-      <Section id="contact">
-        <ContactSection />
-      </Section>
+        <Section id="contact">
+          <ContactSection />
+        </Section>
+      </LazyMotion>
     </Main>
   );
 }
